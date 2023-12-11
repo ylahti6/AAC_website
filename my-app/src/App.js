753d416bf782f34df8f55405c517759110f46481
yannick <himynameisyannick@gmail.com>
2023-12-11 13:22:58 +0000

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,59 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import './styles/variable.css';
+import { SlSocialGithub, SlHome, SlGhost } from 'react-icons/sl'
 
 function App() {
+  // const projects = [
+  //   {
+  //     title: "Libernotes",
+  //     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  //     imageSrc: [Fonts],
+  //     keywords: ["React.js", "Scss"],
+  //   },
+  //   {
+  //     title: "IT GIRL Theme (Blog)",
+  //     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  //     imageSrc: [Fonts],
+  //     keywords: ["React.js","SCSS", "Blog",],
+  //   },
+  //   {
+  //     title: "Testing C# Game",
+  //     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  //     imageSrc: [Fonts],
+  //     keywords: ["React.js", "Sass"],
+  //   },
+  //   {
+  //     title: "FullBoost",
+  //     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  //     keywords: ["React.js", "Sass"],
+  //     imageSrc: [Fonts],
+  //   }
+  // ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrap">
+        <header className="App-header">
+          <div className="nav">
+            <SlGhost className='icon'/>
+            <SlSocialGithub className='icon'/>
+          </div>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1>navs</h1>
+
+          <div className='nav-bottom'>
+            <p>High-quality navigation styling for your site. Made By Yannick.</p>
+            <p>4 Patterns</p>
+          </div>
+        </header>
+
+        {/* <div className='App-content'>
+        {projects.map((project, index) => (
+        <Card key={index} {...project} />
+      ))} */}
+
+        {/* </div> */}
+      </div>
     </div>
   );
 }
---
+++ my-app/src/content/Components/Card.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { PiGithubLogoLight } from "react-icons/pi";
+
+const Card = ({ title, content, keywords, imageSrc }) => {
+  return (
+    <div className="project-card">
+        <div className="project-header">
+        {imageSrc && <img src={imageSrc} className='project-image' alt={`${title} header`} />}
+        </div>
+
+        <div className="project-content">
+        <span>
+          <h6>{title}</h6>
+          <PiGithubLogoLight className='icon'/>
+        </span>
+        <p>{content}</p>
+        
+        {/* Keywords function */}
+        <p className='project-keywords'>
+          {keywords && keywords.map((keyword, index) => (
+            <span key={index} className='keyword'>{keyword}</span>
+          ))}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
