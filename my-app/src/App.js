4654959356ee8c351aa5b7c1d3385aadffe2b1bb
yannick <himynameisyannick@gmail.com>
2023-12-16 13:20:21 +0000

    Fixed the responsive issues

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,10 +5,8 @@ import Card from './content/Components/Card';
 import box1 from './images/box1.png';
 import box2 from './images/box2.png';
 import box3 from './images/box3.png';
-import { SlSocialGithub, SlHome, SlGhost } from 'react-icons/sl';
 
 function App() {
-  const backgroundImageUrl = 'https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=1.2&cAzimuthAngle=180&cDistance=3.6&cPolarAngle=90&cameraZoom=1&color1=%23ff0700&color2=%233d9fdb&color3=%23d0bce1&embedMode=off&envPreset=city&fov=45&gizmoHelper=hide&grain=on&lightType=3d&pixelDensity=1&positionX=-1.4&positionY=0&positionZ=0&reflection=0.1&rotationX=0&rotationY=10&rotationZ=50&shader=defaults&type=waterPlane&uDensity=1.3&uFrequency=5.5&uSpeed=0.4&uStrength=1.4&uTime=0&wireframe=false';
   const projects = [
     {
       title: "Dropdown Simple",
@@ -34,7 +32,7 @@ function App() {
     <div className="App">
       <div className="wrap">
         <header className="App-header">
-          <h1>Your voice, your way, AAC 
+          <h1>Your voice,<br></br>your way, AAC
             <span className='red'>&nbsp;R</span>
             <span className='yellow'>e</span>
             <span className='purple'>i</span>
@@ -44,9 +42,9 @@ function App() {
             <span className='yellow'>i</span>
             <span className='purple'>n</span>
             <span className='blue'>e</span>
-            <span className='green'>d</span>
+            <span className='green'>d</span>.
           </h1>
-          <div className='nav-bottom'>
+          <div className='App-header-links'>
             <p className='link preview'>Preview</p>
             <p className='link learnMore'>Learn More</p>
           </div>
@@ -61,10 +59,10 @@ function App() {
         </div> */}
 
         <div className='footer'>
-          <div className='project'>
-          <p className='content mg'>Project By: <a href='www.yannicklahti.co.uk'>Yannick Lahti</a></p>
-          <p className='attention'>Transform thoughts into eloquent words, shaping boundless communication.</p>
-          </div>
+          <span>
+            <p className='content mg'>Project By: <a href='www.yannicklahti.co.uk'>Yannick Lahti</a></p>
+            <p className='attention'>Transform thoughts into eloquent words, shaping boundless communication.</p>
+          </span>
           <div className='feedback'>
             <p className='mg'>Support and Feedback</p>
             <p className='attention'>contact@liberaac.com</p>
