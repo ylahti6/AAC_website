df2fb6f28a266762e1ce2ea1c6259aa47f00dc07
Yannick Lahti <ylahti6@gmail.com>
2024-01-07 20:41:30 +0000

    Updated the site, building - updates section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,28 +13,24 @@ function App() {
         {/* Hero section */}
         <header className="min-h-screen flex items-center justify-center">
           <h1 className="font-serif text-7xl font-extrabold">
-            Your voice,<br></br>your way, AAC<br></br>{" "}
+            Your voice,<br></br>your way, AAC
             <div className="font-mono">Reimagined.</div>
           </h1>
         </header>
+        
+
+        
+            
+          
 
         {/* Info section */}
         <div className="flex gap-10 text-gray-600">
-          <div
-            className="
           
-          "
-          >
-            <p></p>
-          </div>
           {/* Building section */}
           <div className="w-48">
             <p className="text-gray-400 my-6 text-14">Building</p>
-            <p>
-              Empowering communication effortlessly—our AAC app not only
-              simplifies accessibility for all but also offers a cost-effective
-              solution, ensuring that everyone has the means to express
-              themselves with ease and affordability.
+            <p>Oratio is a cross-platform AAC app for Android and iOS. With a customizable grid of symbols and words, users tap to generate spoken communication. Its compatibility ensures accessibility on various devices, offering a seamless experience for users
+
             </p>
           </div>
 
@@ -53,8 +49,11 @@ function App() {
             <p>Project By: Yannick Lahti</p>
           </div>
         </div>
-        <div className="">
-          <p className="">Now</p>
+        <div className="max-w-2xl my-24">
+          <p className="italic text-gray-400 my-6">What is AAC?</p>
+          <p>
+          AAC stands for Augmentative and Alternative Communication, a set of tools and strategies helping individuals with communication difficulties express themselves. From low-tech options like picture boards to high-tech devices and software, AAC enhances or supplements verbal communication for those with conditions like autism or speech disorders, fostering inclusivity and enabling active participation in various aspects of life.
+          </p>
         </div>
       </div>
     </div>
