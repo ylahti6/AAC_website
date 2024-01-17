9ef549e08a9b80cdbdbc1500371375f1c3f1ba53
Yannick Lahti <ylahti6@gmail.com>
2024-01-17 21:39:23 +0000

    .

--- my-app/src/Components/Footer.js
+++ my-app/src/Components/Footer.js
@@ -3,15 +3,16 @@ import React from "react";
 import { BsInstagram } from "react-icons/bs";
 
 const Footer = () => {
+
   return (
     <div className="footer">
         <h1>if you like what you see, why not</h1>
         <button>Contact Us</button>
         <p>and let's make speech possible.</p>
         <span>
-          <a href=""></a>
-          <a href=""></a>
-          <a href=""></a>
+          <a href=""><BsInstagram /></a>
+          <a href=""><BsInstagram /></a>
+          <a href=""><BsInstagram /></a>
         </span>
     </div>
   );
